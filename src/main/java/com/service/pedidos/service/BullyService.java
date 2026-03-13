52f623e8b144c4172a7ba4ac95119db5dd249160
Kreucyu <elciocleiton72@gmail.com>
2026-03-13 20:54:36 -0300

    feat: Bully parte 1

---
+++ src/main/java/com/service/pedidos/service/BullyService.java
@@ -0,0 +1,9 @@
+package com.service.pedidos.service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BullyService {
+    private List<nodes> nodesList = new ArrayList<>();
+
+}
