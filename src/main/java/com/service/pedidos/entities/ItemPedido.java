1f0d766be4bc3bf7fe955c31bebc72d9db136c5c
Kreucyu <elciocleiton72@gmail.com>
2026-03-13 00:57:54 -0300

    fix: correções feitas nas classes de Entities

--- src/main/java/com/service/pedidos/entities/ItemPedido.java
+++ src/main/java/com/service/pedidos/entities/ItemPedido.java
@@ -7,31 +7,15 @@ import jakarta.persistence.*;
 public class ItemPedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long _id;
-    @Column(name = "id_pedido",  nullable = false)
-    private Long _idPedido;
-    @Column(name = "quantidade", nullable = false)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "id_pedido", nullable = false)
+    private Pedido idPedido;
+
+    @Column(nullable = false)
     private Integer quantidade;
 
-    public ItemPedido(Long id, Long idPedido, Integer quantidade) {
-    this._id = id;
-    this._idPedido = idPedido;
-    this.quantidade = quantidade;
-    }
-
     public ItemPedido() {
-
-    }
-
-    public Long getId() {
-        return _id;
-    }
-
-    public Long getIdPedido() {
-        return _idPedido;
-    }
-
-    public Integer getQuantidade() {
-        return quantidade;
     }
 }
--- src/main/java/com/service/pedidos/entities/Pedido.java
+++ src/main/java/com/service/pedidos/entities/Pedido.java
@@ -4,28 +4,32 @@ import jakarta.persistence.*;
 
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "pedidos")
 public class Pedido {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long _id;
-    @Column(name= "cliente_id", nullable = false, unique = true)
-    private Long _clienteId;
+    private Long id;
+
+    @Column(name= "cliente_id", nullable = false)
+    private Long clienteId;
+
+    @Enumerated(EnumType.STRING)
     @Column(name= "status_pedido",  nullable = false)
-    private StatusPedido _statusDoPedido;
+    private StatusPedido statusDoPedido;
+
     @Column(name= "data_do_pedido",  nullable = false)
-    private LocalDate _dataDoPedido;
+    private LocalDate dataDoPedido;
+
+    @Enumerated(EnumType.STRING)
     @Column(name= "forma_de_pagamento, nullable = false")
-    private FormaDePagamento _formaDePagamento;
-    @OneToMany
-    private ArrayList<ItemPedido> _itens;
+    private FormaDePagamento formaDePagamento;
 
-
+    @OneToMany(mappedBy = "idPedido",  cascade = CascadeType.ALL)
+    private List<ItemPedido> itens =  new ArrayList<ItemPedido>();
 
     public Pedido() {
-
-
     }
 }
