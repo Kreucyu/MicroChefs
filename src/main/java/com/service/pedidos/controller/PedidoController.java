c4da310c2b3f3d07d1e997b9e838ce73cc626e97
Kreucyu <elciocleiton72@gmail.com>
2026-03-13 10:45:24 -0300

    build: método POST criarPedido criado

--- src/main/java/com/service/pedidos/controller/PedidoController.java
+++ src/main/java/com/service/pedidos/controller/PedidoController.java
@@ -1,14 +1,29 @@
 package com.service.pedidos.controller;
 
+import com.service.pedidos.entities.Pedido;
+import com.service.pedidos.service.PedidoService;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequestMapping("/pedidos")
 public class PedidoController {
 
-    @GetMapping("/teste")
-    public String teste() {
-        return "testando API";
+    public final PedidoService pedidoService;
+
+    public PedidoController(PedidoService pedidoService) {
+        this.pedidoService = pedidoService;
+    }
+
+    @PostMapping
+    public ResponseEntity<Pedido> criarPedido(@RequestBody Pedido pedido) {
+        Pedido novoPedido = criarPedido(pedido);
+        return ResponseEntity.status(HttpStatus.CREATED).body(novoPedido);
+    }
+
+    @GetMapping
+    public ResponseEntity<Pedido> consultarPedido(@RequestParam Long id){
+        return ResponseEntity.ok();
     }
 }
--- src/main/java/com/service/pedidos/service/PedidoService.java
+++ src/main/java/com/service/pedidos/service/PedidoService.java
@@ -1,4 +1,10 @@
 package com.service.pedidos.service;
 
+import com.service.pedidos.entities.Pedido;
+
 public class PedidoService {
+
+    public void criarPedido(Pedido pedido){
+        Pe
+    }
 }
