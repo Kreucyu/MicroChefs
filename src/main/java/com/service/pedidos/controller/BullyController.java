189f619353a5f6ee745d2baf607a2d107d013db3
Kreucyu <elciocleiton72@gmail.com>
2026-03-13 21:02:21 -0300

    build: BullyService e NodesInfo implementados

--- src/main/java/com/service/pedidos/controller/BullyController.java
+++ src/main/java/com/service/pedidos/controller/BullyController.java
@@ -1,4 +1,29 @@
 package com.service.pedidos.controller;
 
+import com.service.pedidos.service.BullyService;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/bully")
 public class BullyController {
+
+        private final BullyService bullyService;
+
+        public BullyController(BullyService bullyService) {
+            this.bullyService = bullyService;
+        }
+
+        @PostMapping("/election")
+        public void receiveElection(@RequestBody Integer senderId) {
+            bullyService.receiveElection(senderId);
+        }
+
+        @PostMapping("/coordinator")
+        public void receiveCoordinator(@RequestBody Integer coordinatorId) {
+            bullyService.receiveCoordinator(coordinatorId);
+        }
+
 }
--- src/main/java/com/service/pedidos/service/BullyService.java
+++ src/main/java/com/service/pedidos/service/BullyService.java
@@ -1,9 +1,104 @@
 package com.service.pedidos.service;
 
+import com.service.pedidos.entities.NodesInfo;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class BullyService {
-    private List<nodes> nodesList = new ArrayList<>();
 
-}
+        @Value("${process.id}")
+        private int processId;
+
+        private boolean isCoordinator = false;
+
+        private final RestTemplate restTemplate = new RestTemplate();
+
+        private final List<NodesInfo> cluster = List.of(
+                new NodesInfo(1, "http://localhost:8081"),
+                new NodesInfo(2, "http://localhost:8082"),
+                new NodesInfo(3, "http://localhost:8083")
+        );
+
+        public void startElection() {
+
+            System.out.println("Node " + processId + " iniciou eleição");
+
+            boolean higherNodeAlive = false;
+
+            for (NodesInfo node : cluster) {
+
+                if (node.getId() > processId) {
+
+                    try {
+                        restTemplate.postForObject(
+                                node.getUrl() + "/bully/election",
+                                processId,
+                                String.class
+                        );
+
+                        higherNodeAlive = true;
+
+                    } catch (Exception ignored) {
+                    }
+                }
+            }
+
+            if (!higherNodeAlive) {
+                becomeCoordinator();
+            }
+        }
+
+        public void receiveElection(int senderId) {
+
+            System.out.println("Node " + processId + " recebeu eleição de " + senderId);
+
+            if (processId > senderId) {
+
+                try {
+                    restTemplate.postForObject(
+                            "http://localhost:808" + senderId + "/bully/ok",
+                            processId,
+                            String.class
+                    );
+                } catch (Exception ignored) {}
+
+                startElection();
+            }
+        }
+
+        public void becomeCoordinator() {
+
+            isCoordinator = true;
+
+            System.out.println("Node " + processId + " virou COORDENADOR");
+
+            for (NodesInfo node : cluster) {
+
+                if (node.getId() != processId) {
+
+                    try {
+                        restTemplate.postForObject(
+                                node.getUrl() + "/bully/coordinator",
+                                processId,
+                                String.class
+                        );
+                    } catch (Exception ignored) {}
+                }
+            }
+        }
+
+        public void receiveCoordinator(int coordinatorId) {
+
+            isCoordinator = false;
+
+            System.out.println("Node " + processId + " reconhece " + coordinatorId + " como coordenador");
+        }
+
+        public boolean isCoordinator() {
+            return isCoordinator;
+        }
+    }
